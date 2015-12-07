c99b2a612bb3251a0c5df6119e1cfe38d36161ff
Dylan Beswick <Dylan5797@users.noreply.github.com>
2015-12-07 16:59:59 -0700

    Fixed some bugs, error ignoring :+1:

--- BBCodeParser.py
+++ BBCodeParser.py
@@ -9,15 +9,26 @@ def parse_bbcode(bc):
     parsing_tag = False
     in_code = False
     code_init = False
+    tag_depth = 0
     for x in bc:
         count += 1
         if parsing_tag:
-            current_text = current_text + x
-            if x == ']':
-                tokens_found.append({'id':'tag', 'value':'[' + current_text.split()[0].rsplit('=')[0].replace('[', '').replace(']', '') + ']'})
+            if x == '[':
+                tag_depth += 1
+            if tag_depth == 0:
+                current_text = current_text + x
+            if x == ']' and tag_depth == 0:
+                current_text = current_text[1:-1]
+                attr = {}
+                for x in current_text.split():
+                    try: attr[x.rsplit('=')[0]] = x.rsplit('=')[1]
+                    except: pass
+                tokens_found.append({'id':'tag', 'attributes':attr, 'value':current_text.split()[0].rsplit('=')[0]})
                 parsing_tag = False
                 current_text = ''
                 continue
+            elif x == ']':
+                tag_depth -= 1
             if code_init:
                 in_code = True
                 code_init = False
@@ -25,6 +36,7 @@ def parse_bbcode(bc):
             try: n1 = bc[count]; n2 = bc[count]
             except: n1 = ''; n2 = ''
             if x[0] == '[' and (not (n1 == '[' and n2 == ']')) and (not in_code):
+                tag_depth = 0
                 tokens_found.append({'id':'text', 'value':current_text})
                 current_text = '['
                 parsing_tag = True
@@ -32,6 +44,7 @@ def parse_bbcode(bc):
                     code_init = True
                 continue
             elif in_code and bc[count:count+7] == '[/code]':
+                tag_depth = 0
                 tokens_found.append({'id':'text', 'value':current_text})
                 current_text = '['
                 parsing_tag = True
@@ -40,10 +53,10 @@ def parse_bbcode(bc):
             else:
                 current_text = current_text + x
     tokens_found.append({'id':'text', 'value':current_text})
-    def recur(tokens, dep=0):
+    def compile_tags(tokens, dep=0):
         output = []
         if dep > 0:
-            name = tokens[0]['value'][1:-1]
+            name = tokens[0]['value']
         counted = 0
         skip = 0
         bump = ['']
@@ -58,16 +71,16 @@ def parse_bbcode(bc):
             if dep > 0 and len(bump) == len(tokens):
                 continue
             if dep > 0:
-                if x['value'] == '[/' + name + ']':
+                if x['value'] == '/' + name:
                     break
             if x['id'] == 'text':
                 output.append(x['value'])
             else:
-                re = recur(bump, dep + 1)
+                re = compile_tags(bump, dep + 1)
                 skip = re[1]
-                output.append({'tag':x['value'], 'value':re[0]})
+                output.append({'tag':x['value'], 'value':re[0], 'param':x['attributes']})
         if dep > 0:
             return [output, counted - 1]
         else:
             return output
-    return recur(tokens_found)
+    return compile_tags(tokens_found)
